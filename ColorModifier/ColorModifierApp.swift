a345c2ef4f38983a0b5a588690b1d2baa65dc4a9
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-06-30 15:08:55 +0700

    Initial Commit

---
+++ ColorModifier/ColorModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorModifierApp.swift
+//  ColorModifier
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
